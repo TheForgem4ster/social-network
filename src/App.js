95e1f6b574e6edcc62e05787239f8bf0d041bbb4
TheForgem4ster <maxim.thudercat@gmail.com>
2023-01-26 19:18:31 +0200

    11 lessons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+        <header className='header'>
+          <img src='https://avatanplus.com/files/resources/mid/592133aed4a1815c29b1e317.png'/>
+        </header>
+        <nav className='nav'>
+          <div>
+            <a>Profile</a>
+          </div>
+          <div>
+            <a>Message</a>
+          </div>
+          <div>
+            <a>New</a>
+          </div>
+          <div>
+            <a>Music</a>
+          </div>
+          <div>
+            <a>Setting</a>
+          </div>
+        </nav>
+        <main className='content'>
+          <div>
+            <img src='http://pics.livejournal.com/anchiktigra/pic/001dgz1h'/>
+          </div>
+          <div>
+            <img src='https://st2.depositphotos.com/1040719/8331/i/600/depositphotos_83315530-stock-photo-polar-bear-shaking-his-head.jpg'/>
+          </div>
+          <div>
+            avatar + description
+          </div>
+          <div>
+            post
+            <div>
+              new post
+            </div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </main>
     </div>
   );
 }
