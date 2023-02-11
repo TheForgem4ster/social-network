992ef4673e48278831d339641aab11314db9d246
TheForgem4ster <maxim.thudercat@gmail.com>
2023-02-11 20:45:22 +0200

    60 Lessons. WithRouter to classComponent

--- src/componets/Profile/ProfileContainer.jsx
+++ src/componets/Profile/ProfileContainer.jsx
@@ -3,11 +3,21 @@ import Profile from "./Profile";
 import axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
+import {
+    useLocation,
+    useNavigate,
+    useParams,
+} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+        let userId = this.props.router.params.userId;
+        debugger;
+        if (!userId){
+            userId = 2;
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
             .then(response => {
                 this.props.setUserProfile(response.data);
 
@@ -26,4 +36,21 @@ let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 });
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+let withUrlDataContainerComponent =  withRouter(ProfileContainer);
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
+    }
+
+    return ComponentWithRouterProp;
+}
+
+export default connect(mapStateToProps, {setUserProfile})(withUrlDataContainerComponent);
