0c3088acc29c3ac8b59ed68fd5ebf8520ed28823
TheForgem4ster <maxim.thudercat@gmail.com>
2023-02-26 21:25:23 +0200

    74 Lessons. componentDidUpdate

--- src/componets/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/componets/Profile/ProfileInfo/ProfileStatus.jsx
@@ -24,6 +24,16 @@ class ProfileStatus extends React.Component {
            status: e.currentTarget.value,
        })
     }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(prevProps.status !== this.props.status)
+        {
+            this.setState({
+                status: this.props.status,
+            })
+        }
+    }
+
     render() {
         return (
             <div>
