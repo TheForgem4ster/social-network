e81ffe6d15144f58ed5460c8ae7e613e286aac0a
TheForgem4ster <maxim.thudercat@gmail.com>
2023-02-27 23:59:42 +0200

    75 Lessons. Redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form'
 
 let reduces = combineReducers({
     profilePage: profileReducer,
@@ -12,6 +13,7 @@ let reduces = combineReducers({
     sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reduces, applyMiddleware(thunkMiddleware));
